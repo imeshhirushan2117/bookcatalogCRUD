d2b088faaced9415fbc613e2191860875e4564e4
imeshhirushan2117 <imeshhirushan2117@gmail.com>
2023-06-25 15:38:28 +0530

    added postMapping

--- BackEnd/demo/src/main/java/com/example/demo/controller/BookController.java
+++ BackEnd/demo/src/main/java/com/example/demo/controller/BookController.java
@@ -1,16 +1,19 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "api/v1/book")
 @CrossOrigin
 public class BookController {
+
     @GetMapping("/getBook")
     public String getBook(){
         return "getBook";
+    }
+
+    @PostMapping("/saveBook")
+    public String saveBook(){
+        return "save Book";
     }
 }
